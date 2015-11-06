27172dd09b6098812bab3a799a266ceed74eef25
maxcabourg <max.cabourg@etu.umontpellier.fr>
2015-11-07 00:30:13 +0100

    Pilote mysql jdbc

--- IG3_Ludotheque/src/BDD.java
+++ IG3_Ludotheque/src/BDD.java
@@ -1,10 +1,20 @@
 /*Classe qui va representer notre base de données
  * C'es tavec cette classe que l'on fera les requetes SQL
  */
+
+import com.mysql.*;
+
 public class BDD {
 	
 	BDD(){
-		
+		try {
+			//Chargement du pilote mysql
+		     Class.forName("com.mysql.jdbc.Driver");
+		     System.out.println("Driver O.K.");
+		}
+		catch (Exception e) {
+		      e.printStackTrace();
+		}
 	}
 
 }
