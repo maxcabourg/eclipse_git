dfcdfdb4ceae114dbec3679f0d7dfa0264e09e19
maxcabourg <max.cabourg@etu.umontpellier.fr>
2015-11-08 17:15:36 +0100

    Connexion à la BDD effectuée !

--- IG3_Ludotheque/src/BDD.java
+++ IG3_Ludotheque/src/BDD.java
@@ -2,15 +2,24 @@
  * C'es tavec cette classe que l'on fera les requetes SQL
  */
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import com.mysql.*;
 
 public class BDD {
-	
+	private Connection connection;
 	BDD(){
 		try {
 			//Chargement du pilote mysql
 		     Class.forName("com.mysql.jdbc.Driver");
 		     System.out.println("Driver O.K.");
+		     connection = DriverManager.getConnection("jdbc:mysql://mysql-projet-ludotheque.alwaysdata.net/projet-ludotheque_piscine","114844_admin", "admin");
+		     if (connection != null) {
+		 		System.out.println("You made it, take control your database now!");
+		 	} else {
+		 		System.out.println("Failed to make connection!");
+		 	}
 		}
 		catch (Exception e) {
 		      e.printStackTrace();
