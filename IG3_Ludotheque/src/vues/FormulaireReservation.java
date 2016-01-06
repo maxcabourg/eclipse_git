e23a87a75a1c8735585736e7e575b47b980c3d09
charlene.pizzoni <charlene.pizzoni@162.38.111.63>
2016-01-06 19:56:08 +0100

    ajout des text field et label du formulaire reservation

--- IG3_Ludotheque/src/vues/FormulaireReservation.java
+++ IG3_Ludotheque/src/vues/FormulaireReservation.java
@@ -1,46 +1,70 @@
 package vues;
 
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.ParseException;
 
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.text.MaskFormatter;
 
 import com.toedter.calendar.JDateChooser;
 
-public class FormulaireReservation extends JPanel implements ActionListener{
-	private JComboBox idR;
-	private JComboBox idU;
-	private JComboBox idJeuReserve;
-	private JTextField tableauIdExtensionsReservees;
+public class FormulaireReservation extends JPanel implements ActionListener {
+	private JLabel nomJeuReserve;
 	private JDateChooser dateReservation;
-	private JDateChooser dateRendu;
-	private JCheckBox venuChercher;
-	
-	FormulaireReservation(){
-	//-------------MASK FORMATTER-----------------------------------------------------	
-			try {
-				MaskFormatter date = new MaskFormatter("##/##/####");
-				//-------------TEXT FIELD-----------------------------------------------------
-				idU = new JComboBox();
-				idU.setPreferredSize(new Dimension(100, 25));
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+	private JComboBox tableauExtensions;
+
+	FormulaireReservation() {
+		try {
+	// -------------MASK FORMATTER-----------------------------------------------------
+			MaskFormatter date = new MaskFormatter("##/##/####");
+			Font titre = new Font("Arial", Font.BOLD, 26);
 			
+	// -------------TEXT FIELD-----------------------------------------------------
+			dateReservation = new JDateChooser();
+			dateReservation.setPreferredSize(new Dimension(150, 30));
+			
+			tableauExtensions = new JComboBox<String>();
+			tableauExtensions.setPreferredSize(new Dimension(150, 30));
+			
+			JButton valider = new JButton("Valider");
+			valider.addActionListener((ActionListener) this); //On ajoute au bouton un gestionnaire d'evenements
+		    valider.setActionCommand("Valider"); //On definit le nom de l'evenement envoye par le bouton
+			
+	//-------------LABEL-----------------------------------------------------------
+			JLabel lDateReservation = new JLabel(" Date de réservation :   ");
+			lDateReservation.setLabelFor(dateReservation);
+			
+			JLabel lTableauExtensions = new JLabel(" Extensions réservables avec ce jeu :   ");
+			lTableauExtensions.setLabelFor(tableauExtensions);
+			
+			JLabel tableauExtensions = new JLabel(" Extensions réservables avec ce jeu :   ");
+			tableauExtensions.setLabelFor(tableauExtensions);
+
+			nomJeuReserve = new JLabel();
+			nomJeuReserve.setPreferredSize(new Dimension(150, 30));
+		    
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+	}
 	
+	void chargement(int idJeu){
+		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
