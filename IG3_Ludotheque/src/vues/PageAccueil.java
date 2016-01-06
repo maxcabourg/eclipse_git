17ec47d39987c3b4a02411f71187c9de185a9c09
user <user@tower037>
2016-01-06 17:35:48 +0100

    PAGE ACCUEIL

--- IG3_Ludotheque/src/vues/PageAccueil.java
+++ IG3_Ludotheque/src/vues/PageAccueil.java
@@ -1,16 +1,15 @@
 package vues;
 
 import javax.swing.Box;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class PageAccueil extends JPanel{
 	
 	PageAccueil() {
-		JLabel texte = new JLabel("Ajouter un utilisateur");
-		Box mainLayout = Box.createVerticalBox();
-		mainLayout.add(texte);
-	    add(mainLayout);
+		JLabel logo = new JLabel(new ImageIcon("src\\accueil.JPG"));  
+	    add(logo);
 	}
 	
 }
